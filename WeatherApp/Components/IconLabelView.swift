c9db64eb7beac57ef3504ece8d07a8d58a57db85
Burhan Aliskan <burhan.aliskan@gmail.com>
2022-10-14 22:13:40 +0300

    add initial component

---
+++ WeatherApp/Components/IconLabelView.swift
@@ -0,0 +1,12 @@
+//
+//  IconLabelView.swift
+//  WeatherApp
+//
+//  Created by Burhan Alışkan on 14.10.2022.
+//
+
+import UIKit
+
+class IconLabelView: GenericBaseView<IconLabelViewModel> {
+    
+}
---
+++ WeatherApp/Components/IconLabelViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  IconLabelViewModel.swift
+//  WeatherApp
+//
+//  Created by Burhan Alışkan on 14.10.2022.
+//
+
+import Foundation
+
+class IconLabelViewModel {
+    
+}
