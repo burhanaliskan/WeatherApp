1fb26b8372eb9bab075cb1b75841177aa5b0b2bb
Burhan Aliskan <burhan.aliskan@gmail.com>
2022-10-14 22:04:32 +0300

    add initial component for weather

---
+++ WeatherApp/Components/WeatherComponent/WeatherView.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherView.swift
+//  WeatherApp
+//
+//  Created by Burhan Alışkan on 6.10.2022.
+//
+
+import Foundation
+
+class WeatherView: GenericBaseView<WeatherViewModel> {
+    
+    
+    
+    
+}
